d2c915dc32073f98e4d569662f2a3e7ebdaf4173
Nitin Singh <i.nitinsingh18@gmail.com>
2023-06-17 17:16:35 +0530

    Case 2 - (Form App) AlertDialog box is opened with wrong credentials

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -23,5 +23,22 @@ void main() {
 
       expect(find.byType(HomeScreen), findsOneWidget);
     });
+
+    testWidgets(
+        "Verify alertdialog box is opened with wrong username and password",
+        (tester) async {
+      app.main();
+      //* this method is called to rebuild the widget till all the frames are built
+      await tester.pumpAndSettle();
+
+      await tester.enterText(find.byType(TextFormField).at(0), 'userame');
+      await tester.enterText(find.byType(TextFormField).at(1), 'passord');
+
+      await tester.tap(find.byType(ElevatedButton));
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(AlertDialog), findsOneWidget);
+    });
   });
 }
