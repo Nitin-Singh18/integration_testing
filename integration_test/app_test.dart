dc13b8bd2d94b6f1488842d8ac60c9533d9bdbc8
Nitin Singh <i.nitinsingh18@gmail.com>
2023-06-17 17:10:05 +0530

    Case 1 - (Form App) -Testing if the login is working with correct username and password

---
+++ integration_test/app_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:integration_testing/home_screen.dart';
+import 'package:integration_testing/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group("end to end test", () {
+    testWidgets("Verify login screen with correct username and password",
+        (tester) async {
+      app.main();
+      //* this method is called to rebuild the widget till all the frames are built
+      await tester.pumpAndSettle();
+
+      await tester.enterText(find.byType(TextFormField).at(0), 'username');
+      await tester.enterText(find.byType(TextFormField).at(1), 'password');
+
+      await tester.tap(find.byType(ElevatedButton));
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(HomeScreen), findsOneWidget);
+    });
+  });
+}
